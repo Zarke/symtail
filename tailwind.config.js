9b6e6915bddfba200d849fdb6b202bf5097d818f
Zarke <milanzaric54@gmail.com>
2020-08-11 15:39:36 +0200

    landing page initial layout

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      spacing: {
+        '72' : '18rem',
+        '120': '30rem',
+      },
+      fontSize: {
+        '2xs': '0.35rem',
+      },
+      opacity: {
+        '85' : '85%',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
